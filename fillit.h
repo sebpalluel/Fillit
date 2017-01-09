9b78215fbeecaf225a3e281043955d13cf97bbd6
sebpalluel <sebpalluel@free.fr>
2017-01-09 18:26:45 +0100

    change t_point for unsigned int

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: kda-fons <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/09 12:50:26 by kda-fons          #+#    #+#             */
-/*   Updated: 2017/01/09 14:37:35 by psebasti         ###   ########.fr       */
+/*   Updated: 2017/01/09 18:17:26 by psebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,28 +18,28 @@
 # include <unistd.h>
 # include "libft/libft.h"
 
-typedef	struct	s_point
+typedef	struct		s_point
 {
-	int	x;
-	int	y;
-}		t_point;
+	unsigned int	x;
+	unsigned int	y;
+}					t_point;
 
-typedef	struct	s_tetri
+typedef	struct		s_tetri
 {
-	t_point	coord[4];
-	char	value;
-}		t_tetri;
+	t_point			coord[4];
+	char			value;
+}					t_tetri;
 
-typedef	struct	s_map
+typedef	struct		s_map
 {
-	int	size;
-	char	**array;
-}		t_map;
+	int				size;
+	char			**array;
+}					t_map;
 
-char	*read_tetri(int fd);
-int		check_format(char *str);
-int		check_tetri(char *str);
-int		check_valid(char *str);
-t_tetri	*read_tetri(int fd);
+char				*read_tetri(int fd);
+int					check_format(char *str);
+int					check_tetri(char *str);
+int					check_valid(char *str);
+t_list				*read_tetri(int fd);
 
 #endif

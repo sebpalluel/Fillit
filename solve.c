77e832aa072a789f201913f9412f7a5e132dcf4c
sebpalluel <sebpalluel@free.fr>
2017-01-13 18:21:01 +0100

    new function to convert coord of tetri to index in map

--- solve.c
+++ solve.c
@@ -6,47 +6,120 @@
 /*   By: psebasti <sebpalluel@free.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/10 17:40:49 by psebasti          #+#    #+#             */
-/*   Updated: 2017/01/10 19:00:46 by psebasti         ###   ########.fr       */
+/*   Updated: 2017/01/13 18:13:22 by psebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 void		solve(t_tetri *tet)
 {
+	t_map	map;
 	size_t	size;
 	size_t	flag;
-	char	*map;
 
-	size = calc_min_square(tet) - 1;
-	flag = 0;
-	while (flag != 1)
+	size = calc_min_size(tet) - 1;
+	flag = 1;
+	while (flag)
 	{
 		size++;
-		initmap(&map, size);
-		flag = backtracker(map, tet, size);//here algo return 0 if solution not found, return 1 if solved it
-		free(map);
+		map = initmap(size, NULL);
+		flag = backtracker(map, tet, flag);//here algo return 0 if solution not found, return 1 if solved it
+		free(map.array);
 	}
 }
 
-void		initmap(char *map, size_t size)
+t_map		initmap(size_t size, t_tetri *tet)
 {
-	size_t mapsize;
-	size_t i;
+	size_t 	i;
+	t_map	map;
 
 	i = 0;
-	mapsize = (size * size) + size;
-	map = (char *)ft_memalloc(mapsize + 1);
-	while (i < mapsize)
+	map.size = size;
+	map.map_size = (map.size * map.size) + map.size;
+	map.array = (char *)ft_memalloc(map.map_size + 1);
+	while (i < map.map_size)
 	{
-		if ((i % size) == 0)
-			map[i] = '\n';
+		if ((i % map.size) == 0)
+			map.array[i] = '\n';
 		else
-			map[i] = '.';
+		{
+			if (tet == NULL)
+				map.array[i] = '.';
+		}
 		i++;
 	}
-	map[i] = '\0';
+	map.array[i] = '\0';
+	return (map);
 }
 
-int			backtracker(char *map, t_tetri *tet, size_t size)
+char		tet_value(size_t i, t_tetri *tet)
 {
+}
+
+int			backtracker(t_map map, t_tetri *tet, int flag)
+{
+	int		pos;
+
+	pos = 0;
+	if (tet == NULL)
+		return (print_result(tet));
+	while (pos < (int)ft_strlen(map.array))
+	{
+		if (flag)
+		{
+			erase_tetri(map.array, tet);
+			flag = 0;
+		}
+		if ((flag = put_tetri(map, tet, pos)) == 0)
+		{
+			ft_putstr("\x1b[m");
+			if (backtracker(map, tet->next, flag) == 0)
+				return (1);
+		}
+		pos++;
+	}
+	return (0);
+}
+
+void	erase_tetri(char *array, t_tetri *tet)
+{
+	while (*array)
+	{
+		if (*array == tet->value)
+		{
+			*array = '.';
+		}
+		array++;
+	}
+	ft_putstr("\x1b[41;30m erase\n");
+}
+
+int		put_tetri(t_map map, t_tetri *tet, int pos)
+{
+	int		i;
+	t_map	tmp_map;
+
+	i = 0;
+	tmp_map = initmap(map.size);
+	map.array = map.array + pos;
+	while (tmp_map[i])
+	{
+		if (tet->figure[i] == tet->value)
+		{
+			if (*map.array != '.')
+				return (-1);
+			*map.array = tet->figure[i];
+		}
+		if (tet->figure[i] == '\n')
+		{
+			map.array = map.array + map.size - 4;
+			if (map.array > (tmp_map + map.map_size))
+				return (test_end(tet->figure + i));
+		}
+		i++;
+		map.array++;
+	}
+	ft_putstr("\x1b[43;30m");
+	printf("pos : %d, size : %lu, put piece :\n%s\n",pos,map.size,tmp_map);
+	return (0);
 }

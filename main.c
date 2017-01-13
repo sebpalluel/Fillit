06abf136c11ca9569ffa35e2c089e24a4bee73c7
KevinDaFonseca <kevin091095@outlook.fr>
2017-01-13 18:27:30 +0100

    a completer

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: kda-fons <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/09 12:50:49 by kda-fons          #+#    #+#             */
-/*   Updated: 2017/01/09 14:25:33 by psebasti         ###   ########.fr       */
+/*   Updated: 2017/01/13 18:27:07 by kda-fons         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,9 @@ int	main(int argc, char **argv)
 {
 	if (argc != 2)
 		ft_putstr("Usage: ./fillit 'fillit.map'");
+	t_tetri	*tetri;
+	t_map	**map;
+	tetri = read_tetri(argv[1]);
+	print_map(map);
 	return (0);
 }

18a02db61fbd676920f12153083967348cc16993
sebpalluel <sebpalluel@free.fr>
2017-01-14 19:31:57 +0100

    put function to solve after error checking in main

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: kda-fons <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/09 12:50:49 by kda-fons          #+#    #+#             */
-/*   Updated: 2017/01/14 19:26:17 by psebasti         ###   ########.fr       */
+/*   Updated: 2017/01/14 19:31:18 by psebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,12 @@ int		main(int argc, char **argv)
 	fd = open(argv[1], O_RDONLY);
 	error = read_tetri(&tetri, fd);
 	if (argc != 2 || error)
+	{
 		ft_putstr_fd("error/n",1);
+		return (0);
+	}
+	organize(tetri);
+	solve(tetri);
+	free_tetri(tetri);
 	return (0);
 }

cc5b132f9e91a3f4863303f0c835496b6a4d29f9
sebpalluel <sebpalluel@free.fr>
2017-01-14 18:19:59 +0100

    add open to read fd + modif

--- main.c
+++ main.c
@@ -6,19 +6,22 @@
 /*   By: kda-fons <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/09 12:50:49 by kda-fons          #+#    #+#             */
-/*   Updated: 2017/01/13 18:27:07 by kda-fons         ###   ########.fr       */
+/*   Updated: 2017/01/14 18:17:16 by psebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-int	main(int argc, char **argv)
+int		main(int argc, char **argv)
 {
-	if (argc != 2)
-		ft_putstr("Usage: ./fillit 'fillit.map'");
+	int	fd;
+	int	error;
 	t_tetri	*tetri;
-	t_map	**map;
-	tetri = read_tetri(argv[1]);
-	print_map(map);
+
+	tetri = NULL;
+	fd = open(argv[1], O_RDONLY);
+	tetri = read_tetri(fd);
+	if (argc != 2)
+		ft_putstr_fd("error/n",1);
 	return (0);
 }

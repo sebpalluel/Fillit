67a3d92a1b9f0545b5341b4a3b616713cbb3149d
sebpalluel <sebpalluel@free.fr>
2017-01-16 18:57:06 +0100

    change proto ft_freetab

--- libft/ft_freetab.c
+++ libft/ft_freetab.c
@@ -6,13 +6,13 @@
 /*   By: psebasti <sebpalluel@free.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/16 18:44:35 by psebasti          #+#    #+#             */
-/*   Updated: 2017/01/16 18:53:27 by psebasti         ###   ########.fr       */
+/*   Updated: 2017/01/16 18:56:45 by psebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_freetab(char **tab)
+void	ft_freetab(void **tab)
 {
 	int	i;
 

cd53e2e5f3ce2968f860b70c4c677c938286af22
sebpalluel <sebpalluel@free.fr>
2017-01-21 15:43:20 +0100

    solve error in case buffer[ret - 1] != '\n'

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: kda-fons <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/09 12:50:49 by kda-fons          #+#    #+#             */
-/*   Updated: 2017/01/21 13:46:27 by psebasti         ###   ########.fr       */
+/*   Updated: 2017/01/21 14:25:04 by psebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,24 @@ int		main(int argc, char **argv)
 {
 	int	fd;
 	int	error;
-	t_list	**tetri;
+	t_list	**tet;
 	size_t numtetri;
 
 	numtetri = 0;
-	if(!(tetri = (t_list **)ft_memalloc(sizeof(t_list *))))
+	if(!(tet = (t_list **)ft_memalloc(sizeof(t_list *))))
 		return (0);
-	*tetri = NULL;
 	fd = open(argv[1], O_RDONLY);
-	error = read_tetri(tetri, fd);
+	error = read_tetri(tet, fd);
 	if (argc != 2 || error)
 	{
 		ft_putstr_fd("error\n",1);
-		ft_lstfree(tetri);
-		return (0);
+		ft_lstfree(tet);
+		return (close(fd));
 	}
-	if (!(numtetri = organize(tetri)))
+	if (!(numtetri = organize(tet)))
 		return (0);
 	printf("after organize\n");
-	solve(tetri, numtetri);
-	free_tetri(&tetri);
+	//solve(tetri, numtetri);
+	ft_lstfree(tet);
 	return (0);
 }

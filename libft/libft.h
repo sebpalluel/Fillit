0f4cde29148a211ad6da24c8c43efc8e89cb90b1
sebpalluel <sebpalluel@free.fr>
2017-01-21 13:23:43 +0100

    add ft_lstfree

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: psebasti <sebpalluel@free.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/11 15:50:29 by psebasti          #+#    #+#             */
-/*   Updated: 2017/01/21 13:09:24 by psebasti         ###   ########.fr       */
+/*   Updated: 2017/01/21 13:22:13 by psebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,6 +88,7 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memdup(const void *mem, size_t size);
 void	ft_freetab(char **tab);
 void	**ft_newtab(size_t height, size_t width);
+
 t_list	*ft_lstnew(void const *content, size_t content_size);
 void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
 void	ft_lstdel(t_list **alst, void (*del)(void *, size_t));
@@ -96,4 +97,5 @@ void	ft_lstaddend(t_list **alst, t_list *new);
 void	ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 int		ft_lstlen(t_list *lst);
+void	ft_lstfree(t_list **lst);
 #endif

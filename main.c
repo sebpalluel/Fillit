4a5550a82fea531ee8016721afa16b80b4638340
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2017-01-30 19:05:00 +0100

    usage, error handling

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: kda-fons <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/09 12:50:49 by kda-fons          #+#    #+#             */
-/*   Updated: 2017/01/30 17:40:52 by pciavald         ###   ########.fr       */
+/*   Updated: 2017/01/30 18:39:18 by pciavald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,42 +20,46 @@ static void		free_map_and_tetri(t_map *map, t_list **tet)
 	ft_lstfree(tet);
 }
 
-static void		quit(int error, t_map *map, t_list **tet, int fd)
+static int		quit(int error, t_map *map, t_list **tet, int fd)
 {
-	if (error)
-		ft_putendl_fd("error", 1);
 	if (map && tet)
 		free_map_and_tetri(map, tet);
 	if (fd > 0)
 		close(fd);
+	if (error)
+	{
+		ft_putendl_fd("error", 1);
+		return (ERROR);
+	}
+	return (SUCCESS);
 }
 
-int			main(int argc, char **argv)
+static int		usage(void)
 {
-	int		fd;
-	t_list	**tet;
-	t_map 	*map;
-	char	carac;
+	ft_putendl("usage: ./fillit mapname");
+	return (ERROR);
+}
 
+int				main(int argc, char **argv)
+{
+	int			fd;
+	t_list		**tet;
+	t_map 		*map;
+	char		carac;
+
+	if (argc != 2)
+		return (usage());
 	tet = (t_list **)ft_memalloc(sizeof(t_list *));
 	map = (t_map *)ft_memalloc(sizeof(t_map));
 	if (!tet || !map)
-		return (0);
+		return (ERROR);
 	*tet = NULL;
 	carac = 'A';
 	fd = open(argv[1], O_RDONLY);
-	if (argc != 2 || read_tetri(tet, fd) || fd == -1)
-	{
-		quit(ERROR, map, tet, fd);
-		return (1);
-	}
-	if (!(map->numtetri = organize(tet, carac)))
-	{
-		quit(NOERROR, map, tet, fd);
-		return (0);
-	}
+	if (read_tetri(tet, fd) || fd == -1)
+		return (quit(ERROR, map, tet, fd));
+	if ((map->numtetri = organize(tet, carac)) == 0)
+		return (quit(ERROR, map, tet, fd));
 	solve(tet, map);
-	quit(NOERROR, map, tet, fd);
-	return (0);
+	return (quit(SUCCESS, map, tet, fd));
 }
-

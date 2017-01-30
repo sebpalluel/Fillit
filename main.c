1ec888489cdcc9df52f3301ae2d8d5e5344de84b
sebpalluel <sebpalluel@free.fr>
2017-01-30 17:44:52 +0100

    resolved some leaks with ft_lstdel, still 9 leaks with sujet_1

--- main.c
+++ main.c
@@ -6,16 +6,23 @@
 /*   By: kda-fons <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/09 12:50:49 by kda-fons          #+#    #+#             */
-/*   Updated: 2017/01/28 21:45:10 by psebasti         ###   ########.fr       */
+/*   Updated: 2017/01/30 17:43:06 by psebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
+static void		free_tet_elem(void *tet, size_t elem_size)
+{
+	ft_freetab((char **)((t_tetri *)(tet))->coord);
+	(void)elem_size;
+}
+
 static void		free_map_and_tetri(t_map *map, t_list **tet)
 {
 	free(map->array);
 	free(map->coord);
+	ft_lstdel(tet, free_tet_elem);
 	free(map);
 	ft_lstfree(tet);
 }
@@ -43,6 +50,8 @@ int			main(int argc, char **argv)
 		return (0);
 	solve(tet, map);
 	free_map_and_tetri(map, tet);
+	close(fd);
+	while (42);
 	return (close(fd));
 }
 

4d1ccc26068839fcf1b46de2753847628b51aa01
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2017-01-30 19:05:26 +0100

    norm + statics

--- organize.c
+++ organize.c
@@ -6,18 +6,65 @@
 /*   By: psebasti <sebpalluel@free.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/09 16:14:04 by psebasti          #+#    #+#             */
-/*   Updated: 2017/01/24 22:46:46 by psebasti         ###   ########.fr       */
+/*   Updated: 2017/01/30 18:55:35 by pciavald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-size_t		organize(t_list **tet, char carac)
+static size_t	tet_min_xy(t_list **tet, int *array, size_t xy)
 {
-	size_t	counter;
-	int		*array;
-	size_t  min[2];
-	t_list	*first_tet;
+	size_t		i;
+
+	i = 0;
+	while (i < NUMBLOCKS)
+	{
+		array[i] = TET(tet)->coord[i][xy];
+		i++;
+	}
+	return (ft_intmin(array, NUMBLOCKS));
+}
+
+static size_t	tet_max_xy(t_list **tet, int *array, size_t xy)
+{
+	size_t		i;
+
+	i = 0;
+	while (i < NUMBLOCKS)
+	{
+		array[i] = TET(tet)->coord[i][xy];
+		i++;
+	}
+	return (ft_intmax(array, NUMBLOCKS));
+}
+
+static void		mv_upleft(t_list **tet, size_t min[2])
+{
+	size_t		i;
+
+	i = 0;
+	while (i < NUMBLOCKS)
+	{
+		TET(tet)->coord[i][0] -= min[0];
+		TET(tet)->coord[i][1] -= min[1];
+		i++;
+	}
+}
+
+size_t			calc_min_square(size_t numtetri)
+{
+	size_t		numblocks;
+
+	numblocks = numtetri * NUMBLOCKS;
+	return (ft_sqrt(numblocks));
+}
+
+size_t			organize(t_list **tet, char carac)
+{
+	size_t		counter;
+	int			*array;
+	size_t  	min[2];
+	t_list		*first_tet;
 
 	counter = 0;
 	first_tet = *tet;
@@ -38,51 +85,4 @@ size_t		organize(t_list **tet, char carac)
 	*tet = first_tet;
 	free(array);
 	return (counter);
-}
-
-size_t		tet_min_xy(t_list **tet, int *array, size_t xy)
-{
-	size_t 	i;
-
-	i = 0;
-	while (i < NUMBLOCKS)
-	{
-		array[i] = TET(tet)->coord[i][xy];
-		i++;
-	}
-	return (ft_intmin(array, NUMBLOCKS));
-}
-
-size_t		tet_max_xy(t_list **tet, int *array, size_t xy)
-{
-	size_t 	i;
-
-	i = 0;
-	while (i < NUMBLOCKS)
-	{
-		array[i] = TET(tet)->coord[i][xy];
-		i++;
-	}
-	return (ft_intmax(array, NUMBLOCKS));
-}
-
-void		mv_upleft(t_list **tet, size_t min[2])
-{
-	size_t	i;
-
-	i = 0;
-	while (i < NUMBLOCKS)
-	{
-		TET(tet)->coord[i][0] -= min[0];
-		TET(tet)->coord[i][1] -= min[1];
-		i++;
-	}
-}
-
-size_t		calc_min_square(size_t numtetri)
-{
-	size_t	numblocks;
-
-	numblocks = numtetri * NUMBLOCKS;
-	return (ft_sqrt(numblocks));
 }
